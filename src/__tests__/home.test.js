357d67468ada473d6debcaba993ec5d6cac27aaa
brhanuhailu <hailubrhanu29@gmail.com>
2023-04-19 12:54:47 -0700

    fixed eslint

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -3,13 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from '../components/home';
 import store from '../redux/store';
-import fetchCrypto from '../redux/crypto/cryptoSlice';
-
+import { fetchCrypto } from '../redux/crypto/cryptoSlice';
 
 describe('Home page', () => {
-    beforeEach(async () => {
-        await store.dispatch(fetchCrypto);
-    });
+  beforeEach(async () => {
+    await store.dispatch(fetchCrypto);
+  });
 
   it('should renders home page correctly', () => {
     const tree = renderer
