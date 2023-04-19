e0ea20fd20e3ea4a05142afffdba5f5c1aae5f0d
brhanuhailu <hailubrhanu29@gmail.com>
2023-04-19 12:43:28 -0700

    home.test.js

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -3,9 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from '../components/home';
 import store from '../redux/store';
+import fetchCrypto from '../redux/crypto/cryptoSlice';
 
-describe('Home', () => {
-  it('renders correctly', () => {
+
+describe('Home page', () => {
+    beforeEach(async () => {
+        await store.dispatch(fetchCrypto);
+    });
+
+  it('should renders home page correctly', () => {
     const tree = renderer
       .create(
         <Provider store={store}>
